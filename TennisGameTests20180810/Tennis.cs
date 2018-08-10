5d6205ff2b294d1b93a309e1b8da450724370b58
paul_iphuang <paul_iphunag@tutorabc.com>
2018-08-11 00:02:11 +0800

    TddFirstDay_TennisGame

---
+++ TennisGameTests20180810/Tennis.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TennisGameTests20180810
+{
+    public class Tennis
+    {
+        private int _firstPlayerScore;
+
+        private Dictionary<int, string> _scoreLookUp = new Dictionary<int, string>()
+            {
+                { 0, "Love"},
+                { 1, "Fifteen"},
+                { 2, "Thirty"},
+                { 3, "Forty"}
+            };
+
+        private int _secondPlayerScore;
+        private string _firstPlayerName;
+        private string _secondPlayerName;
+
+        public Tennis(string firstPlayerName, string secondPlayerName)
+        {
+            _firstPlayerName = firstPlayerName;
+            _secondPlayerName = secondPlayerName;
+        }
+
+        public string Score()
+        {
+            if (_firstPlayerScore != _secondPlayerScore)
+            {
+                if (_firstPlayerScore >= 3 && _secondPlayerScore >= 3)
+                {
+                    if (Math.Abs(_firstPlayerScore - _secondPlayerScore) == 1)
+                    {
+                        return (_firstPlayerScore > _secondPlayerScore ? _firstPlayerName : _secondPlayerName) + " Adv";
+                    }
+                    if (Math.Abs(_firstPlayerScore - _secondPlayerScore) > 1)
+                    {
+                        return (_firstPlayerScore > _secondPlayerScore ? _firstPlayerName : _secondPlayerName) + " Win";
+                    }
+                }
+
+                if (_firstPlayerScore > 3 || _secondPlayerScore > 3)
+                {
+                    if (Math.Abs(_firstPlayerScore - _secondPlayerScore) > 1)
+                        return (_firstPlayerScore > _secondPlayerScore ? _firstPlayerName : _secondPlayerName) + " Win";
+                }
+                return _scoreLookUp[_firstPlayerScore] + " " + _scoreLookUp[_secondPlayerScore];
+            }
+
+            if (_firstPlayerScore >= 3)
+            {
+                return "Deuce";
+            }
+            return _scoreLookUp[_firstPlayerScore] + " All";
+        }
+
+        public void FirstPlayerScore()
+        {
+            _firstPlayerScore++;
+        }
+
+        public void SecondPlayerScore()
+        {
+            _secondPlayerScore++;
+        }
+    }
+}
---
+++ TennisGameTests20180810/TennisTests.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGameTests20180810
+{
+    [TestClass]
+    public class TennisGameTests
+    {
+        private readonly Tennis _tennis = new Tennis("Joey", "Tom");
+
+        [TestMethod]
+        public void Love_All()
+        {
+            ScoreShouldBe("Love All");
+        }
+
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            _tennis.FirstPlayerScore();
+            ScoreShouldBe("Fifteen Love");
+        }
+
+        [TestMethod]
+        public void Thirty_Love()
+        {
+            FirstPlayerScoreTimes(2);
+            ScoreShouldBe("Thirty Love");
+        }
+
+        [TestMethod]
+        public void Forty_Love()
+        {
+            FirstPlayerScoreTimes(3);
+            ScoreShouldBe("Forty Love");
+        }
+
+        [TestMethod]
+        public void Love_Fifteen()
+        {
+            _tennis.SecondPlayerScore();
+            ScoreShouldBe("Love Fifteen");
+        }
+
+        [TestMethod]
+        public void Love_Thirty()
+        {
+            SecondPlayerScoreTimes(2);
+            ScoreShouldBe("Love Thirty");
+        }
+
+        [TestMethod]
+        public void Fifteen_All()
+        {
+            FirstPlayerScoreTimes(1);
+            SecondPlayerScoreTimes(1);
+            ScoreShouldBe("Fifteen All");
+        }
+
+        [TestMethod]
+        public void Thirty_All()
+        {
+            FirstPlayerScoreTimes(2);
+            SecondPlayerScoreTimes(2);
+            ScoreShouldBe("Thirty All");
+        }
+
+        [TestMethod]
+        public void Deuce()
+        {
+            FirstPlayerScoreTimes(3);
+            SecondPlayerScoreTimes(3);
+            ScoreShouldBe("Deuce");
+        }
+
+        [TestMethod]
+        public void Joey_Adv()
+        {
+            FirstPlayerScoreTimes(4);
+            SecondPlayerScoreTimes(3);
+            ScoreShouldBe("Joey Adv");
+        }
+
+        [TestMethod]
+        public void Tom_Adv()
+        {
+            FirstPlayerScoreTimes(3);
+            SecondPlayerScoreTimes(4);
+            ScoreShouldBe("Tom Adv");
+        }
+
+        [TestMethod]
+        public void Deuce4_4()
+        {
+            FirstPlayerScoreTimes(4);
+            SecondPlayerScoreTimes(4);
+            ScoreShouldBe("Deuce");
+        }
+
+        [TestMethod]
+        public void Joey_Win()
+        {
+            FirstPlayerScoreTimes(5);
+            SecondPlayerScoreTimes(3);
+            ScoreShouldBe("Joey Win");
+        }
+
+        [TestMethod]
+        public void Joey_Win_4_0()
+        {
+            FirstPlayerScoreTimes(4);
+            SecondPlayerScoreTimes(0);
+            ScoreShouldBe("Joey Win");
+        }
+
+        private void SecondPlayerScoreTimes(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                _tennis.SecondPlayerScore();
+            }
+        }
+
+        private void FirstPlayerScoreTimes(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                _tennis.FirstPlayerScore();
+            }
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            var score = _tennis.Score();
+            Assert.AreEqual(expected, score);
+        }
+    }
+}
